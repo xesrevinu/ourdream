6b3339d471629ca731956a21f8db97c1ebede01c
ke <121441909@qq.com>
2015-03-06 21:52:18 +0800

    Changes to be committed: 	modified:   package.json 	modified:   server/Router/app.js

--- server/Router/app.js
+++ server/Router/app.js
@@ -1,11 +1,16 @@
 var fs = require('fs'),
-	_ = require('lodash');
+	_ = require('lodash'),
+	logger = require('tracer');
 module.exports = function(route, render) {
 	var ctrlPath = config.serverPath + '/Controller';
 	var ctrl = fs.readdirSync(ctrlPath);
 
 	_.forEach(ctrl, function(file) {
-		require('../Controller/'+file).call(this, route, render)
+		try{
+			require('../Controller/'+file).call(this, route, render)
+		}catch(e){
+			logger.colorConsole().error(`Controller/${file}加载失败,只能加载js哦少年`)
+		}
 	});
 	return function*(next) {
 		if (ctrl.indexOf(this.originalUrl) == -1) {
