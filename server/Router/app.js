feb34908032c4c679269dcb2cdaf9c208df5d1c6
ke <121441909@qq.com>
2015-03-06 15:54:12 +0800

    路由中间件,读取Control,注入

--- server/Router/app.js
+++ server/Router/app.js
@@ -1,1 +1,16 @@
-module.exports = {};
+var fs = require('fs'),
+	_ = require('lodash');
+module.exports = function(route, render) {
+	var ctrlPath = config.serverPath + '/Controller';
+	var ctrl = fs.readdirSync(ctrlPath);
+
+	_.forEach(ctrl, function(file) {
+		require('../Controller/'+file).call(this, route, render)
+	});
+	return function*(next) {
+		if (ctrl.indexOf(this.originalUrl) == -1) {
+			this.status = 400;
+			this.body = yield render('404');
+		}
+	}
+};
