3b2871ed1020ee7c28238ad7eea3b1d5dff7e552
可妹 <kemei@Macee.local>
2015-03-09 22:07:46 +0800

    use LRUCache

--- server/Data/index.js
+++ server/Data/index.js
@@ -1,42 +1,39 @@
 var _ = require('lodash'),
 	util = require('util'),
+	LRUCache = require('lrucache'),
 	EventEmitter = require('events').EventEmitter,
 	models = require('../Model');
 
-function Cache(){
-	return {};
-}
-Cache.prototype.get = function (){
-
-}
-Cache.prototype.set = function (){
-
+function Cache(capacity) {
+	var cache = LRUCache(capacity);
+	return cache;
 }
 
 function UseCache(cacheType, options) {
 	EventEmitter.call(this)
 	this.cacheType = cacheType || 'memony';
-	this.options = option || {};
+	this.options = options || {};
 }
-util.inherits(MongoCache, EventEmitter);
+util.inherits(UseCache, EventEmitter);
 
 /**
- * 获取model操作 -> find insert save remove 
- * 缓存 ？ 缓存 ： 执行查询语句， this.setCache 
+ * 获取model操作 -> find insert save remove
+ * 缓存 ？ 缓存 ： 执行查询语句， this.setCache
  */
 UseCache.prototype.toCache = function(model) {
 	this.getCache(this.cacheType);
-	
+	//this.cache 
 	// TODU
 	return model;
 }
 
 UseCache.prototype.getCache = function(cacheType) {
-	switch cacheType{
+	switch (cacheType) {
 		case 'redis':
-			break
-		default 'memony':
-			this.cache = new Cache('memony');
+			break;
+		case 'memony':
+			this.cache = new Cache(10);
+			break;
 	}
 	return this;
 }
