ef48e673354ef291459b3de12e072b66b8465e9e
EEP <coolicer@foxmail.com>
2015-04-30 11:24:37 +0800

    添加webpack

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-# ourdream
+# ourdream 0.0.1
 
 ###采用babel编写es6版
 ***
 
 ###安装：
 * node
-* pm2
+* monogodb
+* redis
 
 ```js
 cd ${project}/public/ && bower install
@@ -17,28 +18,21 @@ cd ${project}/public/ && bower install
 
 ####先编译
 
-开发：
+开发环境：
 
 ```js
 npm run build-watch
 ```
 
+####新建一个tab，运行
+
 ```js
-pm2 start server/dist/app.js --watch
+npm run dev
+
 ```
 
 部署：
 
 ```js
-npm run build
-```
-
-```js
-pm2 start server/dist/app.js -i 0
-```
-
-or
-
-```js
-npm start
+npm run deploy
 ```
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,43 @@
-module.exports = {
-    entry: ['./public/js/app.js'],
+
+var webpack = require('webpack');
+var bower_dir = __dirname + '/public/bower_components';
+
+var config = {
+    // 增加第三方
+    addVendor: function (name, path) {
+        this.resolve.alias[name] = path;
+        this.module.noParse.push(new RegExp('^' + name + '$'));
+    },
+    // 入口文件
+    entry: {
+        app: ['./public/js/app.js'],
+        vendors: ['react']
+    },
+    // 指定别名
+    resolve: {
+        alias: {}
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js')
+    ],
+    // 编译文件
     output: {
-        path:'./public/build',
-        filename: 'bundle.js'
+        path: process.env.NODE_ENV === 'production' ? './public/dist' : './public/build',
+        filename: 'bundle.js',
+        publicPath: '/public'
     },
     module: {
+        noParse: [],
         loaders: [
             { test: /\.js$/, loader: 'jsx-loader' }
         ]
     }
 };
+
+
+// 不加到bundle来
+config.addVendor('react', bower_dir + '/react/react.min.js');
+//config.addVendor('bootstrap', bower_dir + '/bootstrap/dist/js/bootstrap.min.js');
+//config.addVendor('jquery', bower_dir + '/jquery/dist/jquery.min.js');
+
+module.exports = config;
