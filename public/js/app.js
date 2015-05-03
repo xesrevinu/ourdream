28e58e25412464e14a210826b4ddee83ddddacc2
coolicer <coolicer@foxmail.com>
2015-05-03 20:55:44 +0800

    加一个判断

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 var React = require('react');
 var AppComponent = require('./AppComponent.js');
 
-
-React.render(<AppComponent/>, document.getElementById('hi'));
-
+if(document.getElementById('hi')) {
+    React.render(<AppComponent/>, document.getElementById('hi'));
+}
 console.log('Loaded the app component');
