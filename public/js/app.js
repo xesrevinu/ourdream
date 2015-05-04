b481e0976fbaf1a4c628b78dfd89f98001d9bf7f
kee <121441909@qq.com>
2015-05-04 20:26:43 +0800

    根据url渲染react.component

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,25 @@
-var React = require('react');
-var AppComponent = require('./AppComponent.js');
+import React from 'react'
+import AppComponent from './AppComponent'
+import loginComponent from './loginComponent'
 
-if(document.getElementById('hi')) {
-    React.render(<AppComponent/>, document.getElementById('hi'));
+const selfUrl = window.location.pathname;
+const reactList = {
+  '/': [{
+    'hi': < AppComponent />
+  }],
+  '/login': [{
+    'login': < loginComponent /> // < AppComponent />
+  }]
 }
-console.log('Loaded the app component');
+if (reactList[selfUrl]) {
+  const components = reactList[selfUrl]
+  for (let i = 0; i < components.length; i++) {
+    for (let k in components[i]) {
+      let ele = document.getElementById(k)
+      if (ele) {
+        let component = components[i][k]
+        React.render(component, ele)
+      }
+    }
+  }
+}
--- public/js/loginComponent.js
+++ public/js/loginComponent.js
@@ -1,8 +1,8 @@
 import React from 'react'
 export default React.createClass({
   render: function() {
-    return ( <div className = "commentBox" >
-      Hello, world!I am a CommentBox. < /div>
+    return ( <div className = "loginBox" >
+      Hello, world!I am a loginBox. < /div>
     );
   }
 });
